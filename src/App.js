4ea640b929ac429156caaa177c59059498968621
kolja09 <kolja9062005@gmail.com>
2021-09-19 21:19:20 +0300

    real-estate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Navbar from "./component/Navbar";
+import GlobalStyle from "./globalStyle";
+import Hero from "./component/Hero";
+import {SliderData} from "./data/SliderData";
+import Dropdown from "./component/Dropdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const  toggle = () =>{
+        setIsOpen(!isOpen)
+    }
+    return (
+        <>
+            <GlobalStyle/>
+            <Navbar toggle={toggle}/>
+            <Dropdown isOpen={isOpen} toggle={toggle}/>
+            <Hero slides={SliderData}/>
+        </>
+    );
+};
 
 export default App;
